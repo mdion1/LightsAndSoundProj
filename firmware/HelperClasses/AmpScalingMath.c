dc07ba17778d2d69a205e03cae89e15233a5c57d
mldion <matthew.l.dion@gmail.com>
2023-03-12 16:28:24 -0700

    replaced sqrt() in AmpScalingMath.c

--- firmware/HelperClasses/AmpScalingMath.c
+++ firmware/HelperClasses/AmpScalingMath.c
@@ -1,120 +1,151 @@
 #include "AmpScalingMath.h"
 
+/* Private variable declarations */
+
+#define SCALAR_FOR_ROUNDING 0
+#define NUM_CYCLES_OFFSET 0
+
+#define LOG_TABLE_BITDEPTH 6
+#define LOG_TABLE_PRECISION 6
+#define LOG_TABLE_LEN ((uint8_t)(1 << LOG_TABLE_BITDEPTH))
+#define TABLE_IDX_MASK ((uint8_t)((1 << LOG_TABLE_BITDEPTH) - 1))
+
+static const uint8_t NLog2Lookup[1 << LOG_TABLE_BITDEPTH] = {
+    0, 1, 3, 4, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 18, 19,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 34, 35, 35, 36,
+    37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 47, 48, 49, 50, 51,
+    52, 52, 53, 54, 55, 56, 56, 57, 58, 59, 60, 60, 61, 62, 63, 63
+};
+
+static const uint8_t NExp2Lookup[1 << LOG_TABLE_BITDEPTH] = {
+    64, 65, 65, 66, 67, 68, 68, 69, 70, 71, 71, 72, 73, 74, 74, 75,
+    76, 77, 78, 79, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+    91, 91, 92, 93, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106,
+    108, 109, 110, 111, 112, 114, 115, 116, 117, 119, 120, 121, 123, 124, 125, 127
+};
+
+static const uint16_t NLog2SampCntLookup[1 << LOG_TABLE_BITDEPTH] = {
+    384, 320, 283, 256, 235, 219, 204, 192, 181, 171, 163, 155, 147, 140, 134, 128,
+    122, 117, 112, 107, 103, 99, 94, 91, 87, 83, 80, 76, 73, 70, 67, 64,
+    61, 58, 56, 53, 51, 48, 46, 43, 41, 39, 37, 35, 33, 30, 29, 27,
+    25, 23, 21, 19, 17, 16, 14, 12, 11, 9, 8, 6, 4, 3, 1, 0
+};
+
 /* Private function declarations */
+
+static uint16_t NLog2(uint16_t val);
+static uint16_t NPow2(uint16_t val);
+static uint8_t countLeadingZeroesInt8(uint8_t x);
+static uint8_t countLeadingZeroesInt16(uint16_t x);
 static uint32_t abs32(int32_t x);
 static uint16_t square8(uint8_t a);
 static uint16_t mult8x8(uint8_t a, uint8_t b);
 
-#ifndef OPTIMIZE_LATER_NOT_NOW
-#include <math.h>
+/* Public function definitions */
 
 uint8_t AmpToBrightness(int32_t sin, int32_t cos, uint8_t numCycles)
 {
-#define MAX_VAL 90000
-#define SCALAR 0.002
-    sin = (sin < 0) ? -sin : sin;
-    cos = (cos < 0) ? -cos : cos;
-    float sin_f = (sin < MAX_VAL) ? sin : MAX_VAL;
-    float cos_f = (cos < MAX_VAL) ? cos : MAX_VAL;    
-    sin_f = sin_f*sin_f;
-    cos_f = cos_f*cos_f;
-    float hypot = sqrtf(sin_f + cos_f);
-    
-    const float scalar = SCALAR;
-    return roundf(scalar * hypot);
-    //const float scalar = 0.704;
-    //return roundf(scalar * hypot / numCycles);
-}
+    if ((numCycles == 0) || (numCycles > LOG_TABLE_LEN)) {
+        return 0;
+    }
 
-#else
-
-#define GAMMALOOKUP_LEN 64
-#define MASK_LSB_START 0x7fff
-#define GAMMA_LEFTSHIFT 5
-
-#define LOG_TABLE_LEN 64
-#define LOG2_SCALING 
-
-static const uint8_t NLog2Lookup;
-
-
-
-
-uint8_t AmpToBrightness(int32_t sin, int32_t cos, uint8_t numCycles)
-{
-    // assumes inputs of 18bits + sign bit
-    // right-shift 18 bits down to 8 bits, do an 8x8 multiply
+    /** Assumes inputs of 18bits + sign bit.
+     *      -Take absolute value to get rid of sign bit.
+     *      -Right-shift 18 bits down to 8 bits, do an 8x8 multiply
+     *      -Right-shift 1 to prevent overflow.
+     *      -Take fastLog:
+     *          x = 64*log2(hypotenuse squared)
+     *      - divide by 2 to take square root
+     *          x / 2 = 64*log2(hypotenuse)
+     *      -
+     */
     uint16_t sin2 = square8(abs32(sin) >> 10);
     uint16_t cos2 = square8(abs32(cos) >> 10);
     
     // divide by two to prevent overflow, then add. Result is 16 bits max.
-    uint16_t mag2 = (sin2 >> 1) + (cos2 >> 1);
-    
-    uint16_t NLogMag = Nlog2(mag2);
-    
+    uint16_t hypot2 = (sin2 >> 1) + (cos2 >> 1);
+    uint16_t NLogHypot = (NLog2(hypot2) + (1 << LOG_TABLE_BITDEPTH)) / 2;       //add (1<<LOG_TABLE_BITDEPTH) to counteract previous divide-by-two, since 64
+
     // todo: now scale log(amp) based on the number of cycles used from the filter/buffer (so there doesn't need to be amplitude rollup @ startup)
     // for example, subtract a number proportional to the number of samples in the buffer, so that for a low number of cycles log(N) doesn't decrease much
     // (essentially what we're doing is:
     //          log(Amp *64 / NumCycles) = log(amp) + const - log(numCycles)
     //                                   = log(amp) + const - log(numCycles*scalar) + log(scalar)   (where scalar is added to reduce rounding error)
+    //uint16_t LogNumCycles = NLog2(numCycles << SCALAR_FOR_ROUNDING) - SCALAR_FOR_ROUNDING*N; //todo: do I need to bitshift this some amount
     
-    uint16_t LogNumCycles = Nlog2(numCycles << SCALAR_FOR_ROUNDING) - SCALAR_FOR_ROUNDING*N; //todo: do I need to bitshift this some amount
-    
-    
-    // now calculate scalarB * 2 ^ (N log2(Amp))
-    uint8_t newLEDval = NPow2(NLogMag - LogNumCycles + NUM_CYCLES_OFFSET);
-    
-    
+    uint16_t NLog2SampCnt = NLog2SampCntLookup[numCycles - 1];       // index bounds have already been checked
+    return (uint8_t)NPow2(NLogHypot + NLog2SampCnt);
 }
 
+/* Private function definitions */
 
 /* returns base2 logarithm multiplied by N (where N is defined in some macro???) */
-uint16_t Nlog2(uint16_t val)
+uint16_t NLog2(uint16_t val)
 {
-    uint16_t maskMSB = 0x8000;
-    uint16_t maskLSB = MASK_LSB_START;
-    
-    uint16_t msb = 15;
-    uint16_t lsb = 0;
+    int16_t MSBit = 15 - countLeadingZeroesInt16(val);
 
-    /*! \todo look for "count trailing zeroes" and "count leading zeroes" assembly instruction! */
-    while (msb)
-    {
-        if(val & maskMSB) {
-            lsb = NLog2Lookup[val & maskLSB];
-            break;
-        }
-        msb--;
-        maskMSB >>= 1;
-        maskLSB >>= 1;
+    if (MSBit < 0) {
+        return 0;
     }
-    return (msb << GAMMA_LEFTSHIFT) + lsb;      // todo GAMMA_LEFTSHIFT is related to the "N" here
+
+    uint8_t LSBits = 0;
+    if (MSBit >= LOG_TABLE_BITDEPTH) {
+        LSBits = val >> (MSBit - LOG_TABLE_BITDEPTH);
+    }
+    else {
+        LSBits = val << (LOG_TABLE_BITDEPTH - MSBit);    // can't bitshift by a negative number of bits
+    }
+    LSBits &= TABLE_IDX_MASK;
+
+    return ((uint16_t)MSBit << LOG_TABLE_PRECISION) + NLog2Lookup[LSBits];
 }
 
 uint16_t NPow2(uint16_t val)
 {
-
-    uint16_t maskMSB = (0x0f << GAMMA_LEFTSHIFT);
-    uint16_t maskLSB = 0x00f0;      //this is just a placeholder
-    
-    return (1 << (val & maskMSB)) + NPow2Lookup[val & maskLSB];
+    // assumes value between 0 - 1024 (10-bit number)
+    // comes from log table lookup:
+    //      log2(uint16) < 16
+    //      64 * log2(uint16) = (4-bit number) << (6-bit-depth table len) = 10 bit number
+    uint8_t bitshift = val >> (LOG_TABLE_BITDEPTH);
+    uint8_t LSBits = TABLE_IDX_MASK & val;
+    if (bitshift >= LOG_TABLE_PRECISION) {
+        return ((uint16_t)NExp2Lookup[LSBits]) << (bitshift - LOG_TABLE_PRECISION);
+    }
+    else {
+        return ((uint16_t)NExp2Lookup[LSBits]) >> (LOG_TABLE_PRECISION - bitshift);
+    }
 }
 
-static const uint16_t NLog2Lookup[GAMMALOOKUP_LEN] = {
-    //...
-};
+static uint8_t countLeadingZeroesInt16(uint16_t x)
+{
+    uint8_t leadingZeroes = countLeadingZeroesInt8(x >> 8); //check MSByte
+    if (leadingZeroes < 8)
+    {
+        return leadingZeroes;
+    }
+    return countLeadingZeroesInt8((uint8_t)x) + 8;
+}
 
-static const uint16_t NPow2Lookup[GAMMALOOKUP_LEN] = {
-//...
-};
+static uint8_t countLeadingZeroesInt8(uint8_t x)
+{
+    uint8_t pos = 8;
+    uint8_t mask = 0x80;
+    while(pos) {
+        if (x & mask) {
+            break;
+        }
+        pos--;
+        mask >>= 1;
+    }
+    return 8 - pos;
+}
 
-#endif
-
-/* Private function definitions */
 static uint32_t abs32(int32_t x)
 {
     // does NOT check for an input value of 0x80000000
-    return (x < 0) ? -x : x;
+    return (uint32_t)(
+            (x < 0) ? -x : x
+        );
 }
 
 static uint16_t square8(uint8_t a) {
